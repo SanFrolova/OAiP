cad918c8d7b35e6f89a9b75771678d43ac1f0356
AlexandraFrolova <frolovaalexandra2007@gmail.com>
2025-07-31 18:54:09 +0300

    Add files via upload

---
+++ lab7/lab7.cpp
@@ -0,0 +1,18 @@
+﻿// lab7 var13
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	double b = 3, c = 6 * pow(10, -4), a = 6, t, x;
+	for (double i = 8; i <= 24; i = i + 4) {
+		t = (a * i) / ((a * a - b) * exp(a + 1));
+		x = t > (5 * c) ? 4.8 * pow(10, -3) + i * a : a + i * i * t;
+		cout << "\n\nt = " << t << endl << "x = " << x;
+	}
+
+
+
+	return 0;
+}
---
+++ lab7/task7.cpp
@@ -0,0 +1,18 @@
+﻿// task7 Вывести первые 6 натуральных чисел, делителями которых являются числа 3 и 5.
+
+#include <iostream>
+using namespace std;
+int main()
+{ 
+    setlocale(LC_ALL, "RU");
+    int i = 1;
+   
+    for (int n = 0; n < 6;) {
+        if (i % 3 == 0 and i % 5 == 0) {     
+            n++;
+            cout << i << endl;
+        }    
+        i++;
+    }
+    return 0;
+}
